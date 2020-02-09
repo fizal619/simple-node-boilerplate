d02a9742beb8aac035fb3dcfe1d8eb6978021b9f
Stefanos <stefanosu422@gmail.com>
2020-02-09 17:32:07 -0500

    adding checkFunc to check if table is reserved

--- models/reservation.js
+++ models/reservation.js
@@ -2,8 +2,11 @@
 module.exports = (sequelize, DataTypes) => {
   const SequelizeReservation = sequelize.define('Reservation', {
     name: DataTypes.STRING,
-    slot: DataTypes.DATE
+    slot: DataTypes.DATE, 
+    isReserved: false 
   }, {});
+
+
 
   class Reservation extends SequelizeReservation {
     static async all(){ 
--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,10 @@
-
 const app = document.getElementById('app')
-const timo = document.getElementById('time')
+const time = document.getElementById('time')
 
 const obj = {}
 
 
+///eventListeners
 document.getElementById('time').onchange = function (event) {
   // access text property of selected option
   console.log(event.target.value);
@@ -18,18 +18,19 @@ document.getElementById('table').onchange = function (event) {
   obj["Table"] = event.target.value
 }
 
-document.getElementById("name").addEventListener('submit', functSubmit);
+document.getElementById('name').addEventListener('submit', functSubmit);
+
 
 function functSubmit(event) {
   event.preventDefault();
   var msg = document.getElementById("input1").value;
-  obj["Name"] = msg;
+  console.log(msg);
   console.log(obj);
-  
+  obj["Name"] = event.target.value
 }
 
 
-
+///get requests to retrieve reservations 
 const getReservation = () => {
   fetch("http://localhost:3000/reservations", {
     headers: {
@@ -37,12 +38,42 @@ const getReservation = () => {
     },
     method: "GET"
   })
-    .then(resp => resp.json())
-    .then(console.log);
+  .then(resp => resp.json())
+  .then(console.log);
 };
 
 
+//creat func to check if table is reserved 
+//if reserved return table reserved 
+//and remove that slot from option 
+
+const checkIfReserved = () => {
   
-  // call createTimes function to remove timeslots from the ones taken and display them in front-end
-  // export const reservationData = data
+}
+
+//before submit check if the table is reserved if it is alert(reserved)
+// make post request to submit reservation to db and create with div(table) 
+const table = document.getElementById('table').value 
+console.log(table)
+const tables = document.getElementById('tables-row')
+  if(table !== )
+    tables.addEventListener("submit", function(event) {
+      event.preventDefault();
+    fetch('http://localhost:3000/toys',{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify( 
+      {
+        "name": form.name.value,
+        "image": form.image.value,
+        "likes": 0
+      })
+    })
+    .then(response => response.json())
+    .then(addToyCard)
+  })   
+
 
