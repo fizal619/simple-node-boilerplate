a62df3fdcc263bfb1904bc3e0daac1cd54d2d63b
June Hosoya <junehosoya@gmail.com>
2020-02-12 11:15:56 -0500

    Added Calendar boiler plate in the html

--- public/js/main.js
+++ public/js/main.js
@@ -26,11 +26,34 @@ const fetchReservations = () => {
   return axios.get('/reservations')
     .then(response => {
       let data = response.data;
-      
+
       //create date heading
-      let table = document.querySelector("table");
-      generateTableHead(table);
-      
+      // let table = document.querySelector("table");
+      // generateTableHead(table);
+      let myCalendar = new VanillaCalendar({
+        selector: "#myCalendar",
+        date: new Date(),
+        todaysDate: new Date(),
+        pastDates: false,
+        months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        shortWeekday: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+        // availableWeekDays: [
+        //   { day: 'monday', others: values },
+        //   { day: 'tuesday', others: values }
+        // ],
+        // availableDates: [
+        //   { date: '2019-09-15', others: values },
+        //   { date: '2019-09-16', others: values },
+        //   { date: '2019-09-17', others: values },
+        //   { date: '2019-09-25', others: values },
+        //   { date: '2019-09-26', others: values }
+        // ],
+        // datesFilter: true,
+        // onSelect: (data, elem) => {}
+      })
+
+      myCalendar.init();
+
 
 
     })
