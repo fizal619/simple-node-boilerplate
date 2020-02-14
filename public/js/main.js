8c64efb214b1fd7be0ddbcb6388b3c4676ebdca1
June Hosoya <junehosoya@gmail.com>
2020-02-14 16:25:13 -0500

    added time

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,5 @@
 // jest won't error out on DOM calls - where i'll we making a call to the database
 
-// const displayPage = (elem) => {
-//   const home = document.getElementById('homepage');
-//   const upcoming = document.getElementById('upcomingpage');
-//   const reservations = document.getElementById('reservationspage');
-
-//   if(elem === 'reservationspage') {
-//     home.style.display = 'none';
-//     upcoming.style.display = 'none';
-//     reservations.style.display = 'flex';
-//   } else if(elem == 'upcomingpage') {
-//     reservations.style.display = 'none';
-//     home.style.display = 'none';
-//     upcoming.style.display = 'flex';
-//   } else {
-//     reservations.style.display = 'none';
-//     upcoming.style.display = 'none';
-//     home.style.display = 'flex';
-//   }
-// };
-
 let settings = {
   selector: "#myCalendar",
   date: new Date(),
@@ -40,6 +20,7 @@ const fetchReservations = () => {
       let myCalendar = new VanillaCalendar(settings)
       myCalendar.init();
       document.getElementById('date').value = moment().format('MM/DD/YYYY');
+      generateTime();
     })
     .catch(error => console.error(error));
 };
--- public/js/utils.js
+++ public/js/utils.js
@@ -1,60 +1,26 @@
-// define your functions like this so they're testable
-const storeHours = ['1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00', '8:00', '9:00', '10:00'];
-
-// //Generate Dates
-// const generateDates = () => {
-//   let arrayDates = [];
-
-//   for (let i = 0; i < 7; i++) {
-//     let currentDate = new Date();
-//     currentDate.setDate(currentDate.getDate() + i);
-//     arrayDates.push(`${DayAsString(currentDate.getDay())}  ${MonthAsString(currentDate.getMonth())} ${currentDate.getDate()}`);
-//   }
-//   return arrayDates;
-// }
-
-// //generate table
-// function generateTableHead(table) {
-//   let dates = generateDates();
-//   let thead = table.createTHead();
-//   let row = thead.insertRow();
-//   for (let i = 0; i < dates.length; i++) {
-//     let th = document.createElement("th");
-//     let text = document.createTextNode(dates[i]);
-//     th.appendChild(text);
-//     row.appendChild(th);
-//   }
-//   generateTable(table);
-// }
-
-// function generateTable(table) {
-//   for (let element of storeHours) {
-//     let row = table.insertRow();
-//     for (let i = 0; i < storeHours.length; i++) {
-//       let cell = row.insertCell();
-//       let text = document.createTextNode(storeHours[i]);
-//       cell.appendChild(text);
-//     }
-//   }
-// }
 
 
-//Toggle Page function
-
-// const displayPage = (elem) => {
-//   const home = document.getElementById('homepage');
-//   const reservations = document.getElementById('reservationspage');
-
-//   if(elem === 'reservationspage') {
-//     home.style.display = 'none';
-//     reservations.style.display = 'flex';
-//   } else {
-//     reservations.style.display = 'none';
-//     home.style.display = 'flex';
-//   }
-// };
-
-{/* <div class="vanilla-calendar-date vanilla-calendar-date--active vanilla-calendar-date--today vanilla-calendar-date--selected" data-calendar-date="Wed Feb 12 2020 12:17:20 GMT-0500 (Eastern Standard Time)" data-calendar-status="active"><span>12</span></div> */ }
+const timeSlots = [
+  '13:00',
+  '13:30',
+  '14:00',
+  '14:30',
+  '15:00',
+  '15:30',
+  '16:00',
+  '16:30',
+  '17:00',
+  '17:30',
+  '18:00',
+  '18:30',
+  '19:00',
+  '19:30',
+  '20:00',
+  '20:30',
+  '21:00',
+  '21:30',
+  '22:00',
+];
 
 const generateDate = (data) => {
   let date = moment(data.date).format('MM/DD/YYYY');
@@ -62,5 +28,18 @@ const generateDate = (data) => {
 }
 
 const generateTime = () => {
-  //call axios request to generate time..
+  //call axios request to generate time.. but for now a dummy data...
+
+  // get reference to select element
+  let sel = document.getElementById('timeSlots');
+
+  // create text node to add to option element (opt)
+  timeSlots.forEach((time) => {
+    let opt = document.createElement('option');
+    opt.appendChild(document.createTextNode(time));
+    opt.value = time;
+    // add opt to end of select box (sel)
+    sel.appendChild(opt);
+  })
+
 }
